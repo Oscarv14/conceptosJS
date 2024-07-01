059a6bd031b7b19511d69a324a97c40e9a25ed91
Oscar Ventura <oventura115@gmail.com>
2024-07-01 02:38:50 -0300

    Se empezo a trabajar con javascript

---
+++ js/01-variables.js
@@ -0,0 +1,33 @@
+// Esto es un comentario de una linea
+
+/* Esto es un comentario
+de
+multiples 
+lines
+*/
+
+// declarar una variable (var - let - const)
+
+let anioActual = 2024;
+console.log (anioActual);
+
+// modificamos el contenido de una variable
+
+anioActual = 2024 + 2;
+console.log (anioActual);
+document.write(anioActual);
+document.write("<p class='textResaltado'>Esto es un parrafo</p>");
+document.write('Año Actual: '+ anioActual);
+
+// ventanas emergentes
+//alert ("Hola Mundo");
+
+const url = "https://universe.rollingcodeschool.com/"
+
+document.write ('<br>' + url);
+
+// pedir al usuario un numero
+const numero1 = parseInt(prompt('Ingresa un número'));
+console.log(numero1);
+const resultado = numero1 + 30;
+console.log(resultado)
